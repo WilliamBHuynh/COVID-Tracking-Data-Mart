97e51edd8b1c0a7322b23b432b6f68e69de4ed62
syilmaz1226 <syilm068@uottawa.ca>
2021-03-16 10:35:49 -0400

    Update special_measures.py

--- src/backend/special_measures.py
+++ src/backend/special_measures.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 special = pd.read_csv(
-    r"C:\Users\samet\Documents\January_2021\CSI4142\Project_Git\Data\Special_Measures\Restrictions.csv")
+    r"C:\Users\samet\Documents\January_2021\CSI4142\Project_Git\Data\Special_Measures\Special_Measures_Dimension_Final.csv")
 
 f = open(r"C:\Users\samet\Documents\passwordpy.txt", "r")
 password = f.readline()
@@ -23,21 +23,21 @@ curr = conn.cursor()
 curr.execute("""DROP TABLE if exists d_special_measures;
             CREATE TABLE d_special_measures
             (
-            measures_key              INT NOT NULL,
+            surrogate_key              INT NOT NULL,
             begin_date                DATE NOT NULL,
             end_date                  DATE NOT NULL, 
             city                 VARCHAR(10) NOT NULL,
             zone_measures        VARCHAR(10) NOT NULL,
             description          VARCHAR(150) NOT NULL,
-            PRIMARY KEY (measures_key)
+            PRIMARY KEY (surrogate_key)
             );""")
 
 
-sqlInsert = """ INSERT INTO d_special_measures (measures_key, begin_date, end_date, city, zone_measures, description) VALUES (%s,%s,%s,%s,%s,%s) """
+sqlInsert = """ INSERT INTO d_special_measures (surrogate_key, begin_date, end_date, city, zone_measures, description) VALUES (%s,%s,%s,%s,%s,%s) """
 
 for idx, row in special.iterrows():
-    record = (row["surrogate_key"], row["start_date"], row["end_date"], row["city"], row["zone_measures"],
-              row["description"])
+    record = (row["Special_Measures_Surrogate_Key"], row["Start_Date"], row["End_Date"], row["City"], row["Zone_Measures"],
+              row["Description"])
     curr.execute(sqlInsert, record)
 
 conn.commit()
